77d6425d484d734b766741b2876a593d614876a4
Kathy Franco <kfrancoba@mail.fresnostate.edu>
2022-05-16 14:07:44 -0700

    Stamp Program

--- LA_2.py
+++ LA_2.py
@@ -21,30 +21,54 @@ loop through scores
 output = accumultor / total scores
 print output
 """""
-def calculate_average(scores):
-    iterator = 0
-    accumulator = 0
-    student_count = len(scores)
-    print("Length is: ", len(scores))
-    #total = 100 + 80 + 90 + 70 + 50 + 95
-    #print("total: ", total)
-    # while there are more scores:
-    while iterator < len(scores):
-        iterator = iterator + 1
-        # print ("within while loop iterator: ", iterator)
-        print (f"item at index {iterator} is: ", scores[iterator])
-        accumulator = accumulator + scores[iterator]
+# def calculate_average(scores):
+#     iterator = 0
+#     accumulator = 0
+#     student_count = len(scores)
+#     print("Length is: ", len(scores))
+#     #total = 100 + 80 + 90 + 70 + 50 + 95
+#     #print("total: ", total)
+#     # while there are more scores:
+#     while iterator < len(scores):
+#         iterator = iterator + 1
+#         # print ("within while loop iterator: ", iterator)
+#         print (f"item at index {iterator} is: ", scores[iterator])
+#         accumulator = accumulator + scores[iterator]
 
-    print("sum is: ", accumulator)
-    average = accumulator / student_count
-    print("The average of total scores in the class is: ", average)
+#     print("sum is: ", accumulator)
+#     average = accumulator / student_count
+#     print("The average of total scores in the class is: ", average)
 
-output = calculate_average([50, 0, 100, 80, 90, 70, 50, 95, 60, 90, 50, 100, 70, 90])
-print("The average of total scores in the class is: ", output)
+# output = calculate_average([50, 0, 100, 80, 90, 70, 50, 95, 60, 90, 50, 100, 70, 90])
+# print("The average of total scores in the class is: ", output)
 
 
 
-#do our math
+# #do our math
+
+
+"""""
+start
+get the numbers of sheets
+sheets/ 5
+round answer to next number
+output the result to the user
+end
+
+"""""
+import math 
+def calculate(sheet):
+    answer = sheet / 5
+    rounded = round(answer)
+    print("sheet is : ", sheet)
+    print("the answer is: ", answer)
+    print("rounded is: ", math.ceil(rounded))
+    print("===================")
+    return rounded
+
+output = calculate(16)
+
+print("the return statement is: ", output)
 
 
 
